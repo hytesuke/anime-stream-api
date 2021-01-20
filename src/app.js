ff3424e8969d96c2f494990c4b2a445fc1767510
Hytesuke <hytesuke@gmail.com>
2021-01-20 07:14:48 +0100

    (feat): add mongodb / mongo-express / api to docker-compose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-import app from './express';
-import 'dotenv/config';
 
+import 'dotenv/config';
+import app from './express';
+import './database';
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ src/database/index.js
@@ -0,0 +1,55 @@
+'use strict'
+// require mongoose module
+import mongoose from 'mongoose';
+
+// require chalk module to give colors to console text
+import chalk from 'chalk';
+const connected = chalk.bold.cyan;
+const error = chalk.bold.yellow;
+const disconnected = chalk.bold.red;
+const termination = chalk.bold.magenta;
+
+// import config file
+
+// require database URL from properties file
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB
+} = process.env;
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+};
+
+const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+console.log(url);
+// connection to mongodb 
+mongoose.connect(url, options);
+
+// mongodb instance 
+const db = mongoose.connection;
+db.on('connected', () => {
+    console.log(connected("Mongoose default connection is open to", url));
+});
+
+db.on('error', (err) => {
+    console.log(error("Mongoose default connection has occured "+err+" error"));
+    throw err
+});
+
+db.on('disconnected', () => {
+    console.log(disconnected("Mongoose default connection is disconnected"));
+});
+
+process.on('SIGINT', () => {
+    db.close(() => {
+        console.log(termination("Mongoose default connection is disconnected due to application termination"));
+        process.exit(0)
+    });
+});
